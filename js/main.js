c8b5242ed12cdc90cf1aa9d60c1c3b0028ed964f
DonPage <don.page.work@gmail.com>
2015-02-20 13:03:06 -0500

    3way binding

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 /**
  * Created by Chris on 2/20/2015.
  */
+angular.module("termController", ["firebase"])
+    .controller("btnController", function ($scope, $firebase) {
+
+        var ref = new Firebase("https://robotmovement.firebaseio.com");
+        var fb = $firebase(ref);
+
+        var syncObj = fb.$asObject();
+
+        syncObj.$bindTo($scope, "movement");
+
+        //direction function
+        $scope.move = function (direction) {
+            console.log("direction:", direction);
+        };
+
+
+    });
