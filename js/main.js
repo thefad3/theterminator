3f3228f640d6c8bc6de4493b35bf2e89e2636db0
Chris Lynch (vVv F R 0 O T x) <thefad3@fullsail.edu>
2015-02-20 17:00:07 +0000

    adding controller and firebase injection with html

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@
 // Controls are W,S,A,D and X for Stop
 // https://clrobot.firebaseio.com/
 
+angular.module('theTerminator' ['firebase'])
+
+.controller('robotControls' function($scope, $firebase){
+    
+    var ref = new Firebase('https://clrobot.firebaseio.com/');
+    
+    var refControl = ref.child('/direction/');
+    
+    
+    
+})
+
